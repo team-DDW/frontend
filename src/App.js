2a7390ae975f776f46b0ba48ca3bf18fa75b35bd
Macbook <macbook@Macbooks-MacBook-Air.local>
2019-12-15 09:55:14 +0300

    land

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom"
+import Landingpage from "./Components/Landingpage"
+import SideNav from "./Components/SideNav"
+import Portfolio from "./Components/Portfolio"
 
+// import SideNav from './side-nav/SideNav'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    
+     <SideNav/>
+     
+     <Route exact path="/" exact component={Landingpage}/>
+     <Route exact path="/Portfolio" exact component={Portfolio}/>
+    </Router>
   );
 }
 
